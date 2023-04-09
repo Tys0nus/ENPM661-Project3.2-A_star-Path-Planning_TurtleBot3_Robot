194d59376d3e2486d113b8bf3d5a980eecb9c144
Aditya Chaugule <adityachaugule1997@gmail.com>
2023-04-09 15:52:39 -0400

    -feature -integrate -rev01 mk02

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import visualize as vis
-# import a_star_nh_ankur_aditya as astr
+import a_star_nh_ankur_aditya as astr
 import time
 
 if __name__ == "__main__":
@@ -7,22 +7,23 @@ if __name__ == "__main__":
     print("-----------------------")
     print("Path Planning: A star Turtlebot3") 
     print("-----------------------")
+    scaled_val = 200
     x, y, th = [float(x) for x in input("Enter the Start Coordinates: ").split()] 
     
-    if not (0 <= x < 6) or not (0 <= y < 2) or not (0 <= th < 360 and th % 30 == 0):
+    if not (-0.5 <= x < 5.5) or not (-1 <= y < 1) or not (0 <= th < 360 and th % 15 == 0):
         raise ValueError("Start coordinates are not acceptable")
     
-    x = 0.5 + x
-    y = 1 - y
+    x = (0.5 + x)*scaled_val
+    y = (1 - y)*scaled_val
 
     start = (x,y, th)
 
     x, y, th = [float(x) for x in input("Enter the Goal Coordinates: ").split()] 
-    if not (0 <= x < 6) or not (0 <= y < 2) or not (0 <= th < 360 and th % 30 == 0):
+    if not (-0.5 <= x < 5.5) or not (-1 <= y < 1) or not (0 <= th < 360 and th % 15 == 0):
         raise ValueError("Goal coordinates are not acceptable")
     
-    x = x + 0.5
-    y = 1 - y
+    x = (x + 0.5)*scaled_val
+    y = (1 - y)*scaled_val
     goal = (x,y, th)
 
     cl = float(input("Enter a Clearance value: "))
@@ -30,11 +31,9 @@ if __name__ == "__main__":
     rpm_1, rpm_2 = [int(x) for x in input("Enter rpm_1 and rpm_2 : ").split()] 
     if not (0 <= rpm_1 <= 250) or not(0 <= rpm_2 <= 250):
         raise ValueError("Wheel RPM is not acceptable")
-    
-    scaled_val = 200
-    
+
     root, canvas = vis.main(start,goal,cl,scaled_val)
-    # astr.main(root, canvas, start, goal,rpm_1,rpm_2)
+    astr.main(root, canvas, start, goal,rpm_1,rpm_2)
 
     end_time = time.time()
     runtime = end_time - start_time
